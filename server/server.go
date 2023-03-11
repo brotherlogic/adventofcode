a807e218e6e67535e04a117fe6698e01b7d38c94
Simon Tucker <brotherlogic@gmail.com>
2023-03-10 21:12:17 -0800

    Finalised upload plan. This closes #84

--- server/server.go
+++ server/server.go
@@ -6,6 +6,10 @@ import (
 
 	pb "github.com/brotherlogic/adventofcode/proto"
 	aspb "github.com/brotherlogic/adventserver/proto"
+	rspb "github.com/brotherlogic/rstore/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/brotherlogic/goserver/utils"
 
@@ -25,6 +29,21 @@ type Server struct{}
 
 func (s *Server) localSolve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
 	return nil, fmt.Errorf("No local solves yet")
+}
+
+func (s *Server) Upload(ctx context.Context, req *pb.UploadRequest) (*pb.UploadResponse, error) {
+	conn, err := grpc.Dial("rstore.rstore:8080", grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	client := rspb.NewRStoreServiceClient(conn)
+	_, err = client.Write(ctx, &rspb.WriteRequest{
+		Key:   fmt.Sprintf("adventofcode/data/%v-%v", req.GetYear(), req.GetYear()),
+		Value: &anypb.Any{Value: []byte(req.GetData())},
+	})
+
+	return &pb.UploadResponse{}, err
 }
 
 func (s *Server) Solve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
