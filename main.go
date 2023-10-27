e92b94d5ec27e0ca61a1a17f93e144d1c297bfd1
Simon Tucker <brotherlogic@gmail.com>
2023-10-26 20:29:49 -0700

    Built first pass at the server. This closes #92

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -13,7 +10,6 @@ import (
 	"github.com/brotherlogic/adventofcode/server"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 
 	pb "github.com/brotherlogic/adventofcode/proto"
 )
@@ -28,48 +24,13 @@ func main() {
 
 	s := &server.Server{}
 
-	// Load the server certificate and its key
-	serverCert, err := tls.LoadX509KeyPair("sp/server.pem", "sk/server.key")
-	if err != nil {
-		log.Fatalf("failed to load server certificate and key. %s.", err)
-	}
-	log.Printf("GOT: %v", serverCert.Certificate)
-	data, err := ioutil.ReadFile("sp/server.pem")
-	log.Printf("READ: %v, %v", string(data), err)
-
-	// Load the CA certificate
-	trustedCert, err := ioutil.ReadFile("cp/cacert.pem")
-	if err != nil {
-		log.Fatalf("failed to load trusted certificate. %s.", err)
-	}
-
-	// Put the CA certificate to certificate pool
-	certPool := x509.NewCertPool()
-	if !certPool.AppendCertsFromPEM(trustedCert) {
-		log.Fatal("failed to append trusted certificate from pem to certificate pool")
-	}
-
-	// Create the TLS configuration
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-		RootCAs:      certPool,
-		ClientCAs:    certPool,
-		MinVersion:   tls.VersionTLS13,
-		MaxVersion:   tls.VersionTLS13,
-	}
-
-	// Create a new TLS credentials based on the TLS configuration
-	cred := credentials.NewTLS(tlsConfig)
-	log.Printf("made cred: %v -> %v", string(trustedCert), cred)
-
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen on port %v: %v", *port, err)
 	}
-	gs := grpc.NewServer(grpc.Creds(cred))
-	//gs := grpc.NewServer()
-	pb.RegisterAdventServerServiceServer(gs, s)
-	log.Printf("secure server listening at %v", lis.Addr())
+	gs := grpc.NewServer()
+	pb.RegisterAdventOfCodeServiceServer(gs, s)
+	log.Printf("Server listening at %v", lis.Addr())
 
 	// Setup prometheus export
 	http.Handle("/metrics", promhttp.Handler())
--- server/server.go
+++ server/server.go
@@ -5,31 +5,15 @@ import (
 	"fmt"
 
 	pb "github.com/brotherlogic/adventofcode/proto"
-	aspb "github.com/brotherlogic/adventserver/proto"
 	rspb "github.com/brotherlogic/rstore/proto"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/anypb"
-
-	"github.com/brotherlogic/goserver/utils"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-)
-
-var (
-	//Backlog - the print queue
-	count = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "adventofcode_count",
-		Help: "The number of overall requests",
-	})
 )
 
 type Server struct{}
-
-func (s *Server) localSolve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
-	return nil, fmt.Errorf("No local solves yet")
-}
 
 func (s *Server) Upload(ctx context.Context, req *pb.UploadRequest) (*pb.UploadResponse, error) {
 	conn, err := grpc.Dial("rstore.rstore:8080", grpc.WithInsecure())
@@ -40,7 +24,7 @@ func (s *Server) Upload(ctx context.Context, req *pb.UploadRequest) (*pb.UploadR
 	client := rspb.NewRStoreServiceClient(conn)
 
 	_, err = client.Write(ctx, &rspb.WriteRequest{
-		Key:   fmt.Sprintf("adventofcode/data/%v-%v", req.GetYear(), req.GetYear()),
+		Key:   fmt.Sprintf("adventofcode/data/%v-%v-%v", req.GetYear(), req.GetDay(), req.GetPart()),
 		Value: &anypb.Any{Value: []byte(req.GetData())},
 	})
 
@@ -48,21 +32,9 @@ func (s *Server) Upload(ctx context.Context, req *pb.UploadRequest) (*pb.UploadR
 }
 
 func (s *Server) Solve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
-	count.Inc()
+	return nil, status.Errorf(codes.Unimplemented, "Haven't done this yet")
+}
 
-	resp, err := s.localSolve(ctx, req)
-	if err == nil {
-		return resp, err
-	}
-
-	conn, err := utils.LFDialServer(ctx, "adventserver")
-	if err != nil {
-		return nil, err
-	}
-	client := aspb.NewAdventServerServiceClient(conn)
-	val, err := client.Solve(ctx, &aspb.SolveRequest{Year: req.GetYear(), Day: req.GetDay(), Part: req.GetPart()})
-	if err != nil {
-		return nil, err
-	}
-	return &pb.SolveResponse{Answer: val.GetAnswer(), StringAnswer: val.GetStringAnswer(), BigAnswer: val.GetBigAnswer()}, nil
+func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "Haven't done this yet")
 }
