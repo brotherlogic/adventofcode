f4cd8bf7b2e964ffb9ab988f517de541d3eaf5ca
brotherlogic <brotherlogic@gmail.com>
2023-11-15 06:05:25 -0800

    Huh

--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"context"
 	"fmt"
+	"net"
+	"sync"
 
 	pb "github.com/brotherlogic/adventofcode/proto"
 	rspb "github.com/brotherlogic/rstore/proto"
@@ -13,7 +15,21 @@ import (
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-type Server struct{}
+type Server struct {
+	years   map[int32]bool
+	solvers map[string]bool
+	gs      *grpc.Server
+}
+
+func (s *Server) Run(pn int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", pn))
+	if err != nil {
+		return err
+	}
+
+	s.gs = grpc.NewServer()
+	pb.RegisterAdventOfCodeServiceServer()
+}
 
 func (s *Server) Upload(ctx context.Context, req *pb.UploadRequest) (*pb.UploadResponse, error) {
 	conn, err := grpc.Dial("rstore.rstore:8080", grpc.WithInsecure())
@@ -32,9 +48,41 @@ func (s *Server) Upload(ctx context.Context, req *pb.UploadRequest) (*pb.UploadR
 }
 
 func (s *Server) Solve(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "Haven't done this yet")
+	wg := &sync.WaitGroup{}
+
+	var resp *pb.SolveResponse
+	for callback, _ := range s.solvers {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			conn, err := grpc.Dial(callback, grpc.WithInsecure())
+			if err != nil {
+				return
+			}
+			defer conn.Close()
+
+			client := pb.NewSolverServiceClient(conn)
+			res, err := client.Solve(ctx, req)
+			if err != nil {
+				return
+			}
+
+			resp = res
+		}()
+	}
+
+	if resp == nil {
+		return resp, status.Errorf(codes.NotFound, "unable to find solver for %v", req)
+	}
+
+	return resp, nil
 }
 
 func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "Haven't done this yet")
+	for _, year := range req.GetYears() {
+		s.years[year] = true
+	}
+	s.solvers[req.GetCallback()] = true
+	return &pb.RegisterResponse{}, nil
 }
