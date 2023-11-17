ffd2c32a1e05fc63f1d257716ada7463cbd0a92a
brotherlogic <brotherlogic@gmail.com>
2023-11-17 08:41:42 -0600

    Fixed tests

--- solvers/2015/day2.py
+++ solvers/2015/day2.py
@@ -1,2 +1,19 @@
+def solve21(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num2*2+num3*2 + num1*num2*num3
+    elif num2 > num1 and num2 > num3:
+        return num1*2+num3*2 + num1*num2*num3
+    else:
+        return num1*2+num2*2 + num1*num2*num3
+
 def SolveDay2Part1(instructions):
+    for line in instructions.splitlines():
+        pieces = line.split("x")
+        num1 = int(pieces[0])
+        num2 = int(pieces[1])
+        num3 = int(pieces[2])
+        solve21(num1,num2,num2)
+    return -1
+
+def SolveDay2Part2(instructions):
     return -1
