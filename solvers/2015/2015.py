c44fa4bb8806abead925d85a56c0b902f86a3057
Simon Tucker <brotherlogic@gmail.com>
2023-11-16 20:31:20 -0800

    return proto

--- solvers/2015/2015.py
+++ solvers/2015/2015.py
@@ -13,7 +13,7 @@ import day1
 class SolverService(advent_pb2_grpc.SolverServiceServicer):
     def Solve(self, request, context):
         if request.year == 2015 and request.day == 1 and request.part == 1:
-            return day1.SolveDay1Part1(request.data)
+            return advent_pb2.SolveResponse(answer=day1.SolveDay1Part1(request.data))
 
         return None
 
