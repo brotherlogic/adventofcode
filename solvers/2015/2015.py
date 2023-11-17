1ed782531f70746bf16d505fdc6bb239878fcdc3
Simon Tucker <brotherlogic@gmail.com>
2023-11-16 20:24:20 -0800

    Fix callback

--- solvers/2015/2015.py
+++ solvers/2015/2015.py
@@ -20,7 +20,7 @@ class SolverService(advent_pb2_grpc.SolverServiceServicer):
 def register():
     channel = grpc.insecure_channel('adventofcode.adventofcode:8082')
     stub = advent_pb2_grpc.AdventOfCodeInternalServiceStub(channel)
-    response = stub.Register(advent_pb2.RegisterRequest(year=2015, callback="adventofcode.adventofcode-solver-2015:8080"), timeout=1)
+    response = stub.Register(advent_pb2.RegisterRequest(year=2015, callback="adventofcode-solver-2015.adventofcode:8080"), timeout=1)
     print("response = " + str(response))
     threading.Timer(60, register).start()
 
