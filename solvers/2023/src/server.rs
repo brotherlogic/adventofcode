3d4bab2349a21fe70cb250c7885810598a3bf6e0
Simon Tucker <brotherlogic@gmail.com>
2023-12-01 11:47:59 -0800

    Working on the rust version

--- solvers/2023/src/server.rs
+++ solvers/2023/src/server.rs
@@ -1,32 +1,33 @@
 use tonic::{transport::Server, Request, Response, Status};
-use solver::solver__service_server::{Solve, SolveServicerServer};
-use solver::{SolveResponse, SolveRequest};
-mod solver;  
+use solver::{SolveResponse, SolveRequest, solver_service_server::{SolverService, SolverServiceServer}};
+pub mod solver {
+    tonic::include_proto!("adventofcode");
+} 
 
 // defining a struct for our service
 #[derive(Default)]
-pub struct Server {}
+pub struct RServer {}
 
 // implementing rpc for service defined in .proto
 #[tonic::async_trait]
-impl Solve for Server {
-    async fn send(&self,request:Request<SolveRequest>)->Result<Response<SolveResponse>,Status>{
+impl SolverService for RServer {
+    async fn solve(&self,_request:Request<SolveRequest>)->Result<Response<SolveResponse>,Status>{
         Ok(Response::new(SolveResponse{
-                answer:"hello",
+                string_answer:"hello".to_string(),
+                big_answer:0,
+                answer:10,
         }))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-// defining address for our service
     let addr = "[::1]:8080".parse().unwrap();
-// creating a service
-    let solver = Solver::default();
+    let solver = RServer::default();
     println!("Server listening on {}", addr);
 // adding our service to our server.
     Server::builder()
-        .add_service(SolveServer::new(solver))
+        .add_service(SolverServiceServer::new(solver))
         .serve(addr)
         .await?;
     Ok(())
