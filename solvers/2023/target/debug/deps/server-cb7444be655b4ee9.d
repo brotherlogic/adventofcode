f4a77d136458128523f94b17b98311e7961a2db7
brotherlogic <brotherlogic@gmail.com>
2023-11-30 17:16:50 -0800

    Adding in rust

---
+++ solvers/2023/target/debug/deps/server-cb7444be655b4ee9.d
@@ -0,0 +1,6 @@
+/home/brotherlogic/go/src/github.com/brotherlogic/adventofcode/solvers/2023/target/debug/deps/server-cb7444be655b4ee9: src/server.rs src/solver.rs
+
+/home/brotherlogic/go/src/github.com/brotherlogic/adventofcode/solvers/2023/target/debug/deps/server-cb7444be655b4ee9.d: src/server.rs src/solver.rs
+
+src/server.rs:
+src/solver.rs:
