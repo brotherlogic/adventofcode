e687eef31d7b9444f1c4ca48af80812d8a16265a
Simon Tucker <brotherlogic@gmail.com>
2023-12-01 13:23:31 -0800

    Registers on startup

--- solvers/2023/src/server.rs
+++ solvers/2023/src/server.rs
@@ -1,5 +1,7 @@
 use tonic::{transport::Server, Request, Response, Status};
 use solver::{SolveResponse, SolveRequest, solver_service_server::{SolverService, SolverServiceServer}};
+use solver::advent_of_code_internal_service_client::AdventOfCodeInternalServiceClient;
+use solver::RegisterRequest;
 pub mod solver {
     tonic::include_proto!("adventofcode");
 } 
@@ -20,12 +22,31 @@ impl SolverService for RServer {
     }
 }
 
+async fn register() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Registering 2023 Solver");
+
+    let mut client = AdventOfCodeInternalServiceClient::connect("http://adventofcode.adventofcode:8080").await?;
+
+    let request = tonic::Request::new(RegisterRequest {
+        year: 2023,
+        callback: "adventofcode-solver-2023.adventofcode:8080".to_string(),
+    });
+
+    let response = client.register(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:8080".parse().unwrap();
     let solver = RServer::default();
     println!("Server listening on {}", addr);
-// adding our service to our server.
+
+    register().await?;
+
     Server::builder()
         .add_service(SolverServiceServer::new(solver))
         .serve(addr)
