5c4917be214961fae6a063b5f0ff5855f59612d3
Simon Tucker <brotherlogic@gmail.com>
2023-12-01 12:23:42 -0800

    Fixes docker build

--- solvers/2023/build.rs
+++ solvers/2023/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../proto/advent.proto")?;
+    tonic_build::compile_protos("advent.proto")?;
     Ok(())
 }
