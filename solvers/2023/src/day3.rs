f07ebbd14a8be8ba521795a5e796d1f93b9a48e5
Simon Tucker <brotherlogic@gmail.com>
2023-12-03 10:37:01 -0800

    Fixed day3

--- solvers/2023/src/day3.rs
+++ solvers/2023/src/day3.rs
@@ -4,9 +4,11 @@ struct Board {
     symbols: Vec<Symbol>,
 }
 
+#[derive(Debug)]
 struct Number {
     value: u32,
     x: u32,
+    xe: u32,
     y: u32,
 }
 
@@ -34,11 +36,18 @@ fn build_board(board: String) -> Board {
                 if curr_num.len() > 0 {
                     let xval = u32::try_from(curr_num.len()).ok().unwrap();
                     let gnum = curr_num.parse::<u32>().unwrap();
-                    b.nums.push(Number{value: gnum, x: x-xval, y: y});
+                    b.nums.push(Number{value: gnum, x: x-xval, xe: x-1,y: y});
                     curr_num = "".to_string();
                 }
                 // pass
             } else {
+                // we might have a symbol after a number
+                if curr_num.len() > 0 {
+                    let xval = u32::try_from(curr_num.len()).ok().unwrap();
+                    let gnum = curr_num.parse::<u32>().unwrap();
+                    b.nums.push(Number{value: gnum, x: x-xval, xe: x-1,y: y});
+                    curr_num = "".to_string();
+                }
                 b.symbols.push(Symbol{symbol: c.to_string(), x: x, y: y});
             }
 
@@ -50,11 +59,46 @@ fn build_board(board: String) -> Board {
     return b;
 }
 
+fn safe_sub(val: u32) -> u32 {
+    if val == 0 {
+        return 0
+    }
+    return val-1
+}
+
+fn fits(num: &Number, board: &Board) -> bool {
+    for symbol in &board.symbols {
+        //is it to the left?
+        if symbol.y == num.y && num.x != 0 && symbol.x == num.x-1 {
+            return true
+        }
+
+        //is it to the right?
+        if symbol.y == num.y && symbol.x == num.x+num.xe+1 {
+            return true
+        }
+
+        // is it on the top part
+        if num.y != 0 && symbol.y == num.y-1 && symbol.x >= safe_sub(num.x)  && symbol.x <= num.xe+1 {
+            return true
+        }
+
+             // is it on the bottom part
+             if symbol.y == num.y+1 && symbol.x >= safe_sub(num.x)  && symbol.x <= num.xe+1 {
+                return true
+            }
+    }
+
+    return false
+}
+
 pub fn solve_day3_part1(board: String) -> u32 {
     let mut snum = 0;
-    let mut board = build_board(board);
-    for num in board.nums {
+    let  board = build_board(board);
+    for num in &board.nums {
+        if fits(&num, &board) {
         snum += num.value;
+        }
     }
     return snum;
 }
