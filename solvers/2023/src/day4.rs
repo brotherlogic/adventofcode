7f16c72732453c05f930aeb8b4cb8d1f89b11458
Simon Tucker <brotherlogic@gmail.com>
2023-12-04 09:46:52 -0800

    Fixed for part 2

--- solvers/2023/src/day4.rs
+++ solvers/2023/src/day4.rs
@@ -65,6 +65,36 @@ pub fn solve_day4_part1(data: String) -> u32 {
     return total;
 }
 
+pub fn solve_day4_part2(data: String) -> u32 {
+    let mut total = 0;
+    let mut mapper: HashMap<u32,u32> = HashMap::new();
+
+    for game in build_scratch(data).iter().rev() {
+        let mut gtotal = 0;
+        for winner in &game.winners {
+            for mine in &game.hand {
+                if mine == winner {
+                    gtotal += 1;
+                    break;
+                }
+            }
+        }
+
+        let mut ntotal: u32 = 1;
+        for gval in game.card_number+1..game.card_number+gtotal+1 {
+            ntotal += mapper[&gval]
+        }
+        mapper.insert(game.card_number, ntotal);
+ 
+    }
+
+    for (key, value) in mapper {
+        total += value;
+    }
+
+    return total;
+}
+
 #[cfg(test)]
 mod testsca {
     use super::*;
@@ -88,5 +118,17 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".to_string(), 13);
         for (case, answer) in &test_cases {
             assert_eq!(&solve_day4_part1(case.to_string()),answer);
         } 
+    }
+
+    #[test]
+    fn part2_tests() {
+        let case = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".to_string();
+        let answer: u32 = 30;
+        assert_eq!(solve_day4_part2(case.to_string()), answer)
     }
 }
