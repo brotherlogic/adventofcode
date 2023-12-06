8cf42b07adff993f834c0d7f83cbf1588f8fe22b
Simon Tucker <brotherlogic@gmail.com>
2023-12-06 07:39:09 -0800

    Finished part 1

--- solvers/2023/src/day6.rs
+++ solvers/2023/src/day6.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Race {
     time: i64,
     distance: i64,
@@ -13,7 +14,8 @@ fn build_races(data: String) -> Vec<Race> {
     iter2.next();
 
     let mut races = Vec::new();
-    for t in iter1.next() {
+    for t in iter1 {
+        println!("HERE");
         let d = iter2.next();
         races.push(Race{
             time: t.parse::<i64>().unwrap(), 
@@ -24,11 +26,30 @@ fn build_races(data: String) -> Vec<Race> {
     return races;
 }
 
+fn solve_race(race: Race) -> i32 {
+    let a: f64 = -1_f64;
+    let b: f64 = race.time as f64;
+    let c: f64 = -1_f64 * race.distance as f64;
+    let lower_bound = (-b + f64::sqrt(b*b - 4_f64*a*c)) / (2_f64*a);
+    let upper_bound = (-b - f64::sqrt(b*b - 4_f64*a*c)) / (2_f64*a);
+
+    println!("HERE {} {}", upper_bound.ceil(), lower_bound.ceil());
+    let val =  (upper_bound.floor() - lower_bound.ceil()) as i32;
+    if upper_bound.fract() == 0.0 {
+        return val-1;
+    }
+    return 1+val;
+}
 
 pub fn solve_day6_part1(data: String) -> i32 {
     let mut solution = 1;
 
     let races = build_races(data);
+    println!("Running {:?}", races);
+    for race in races {
+
+        solution *= solve_race(race);
+    }
 
     return solution;
 }
