4c569a6c7ded6046f5067175bb14232901cc4978
Simon Tucker <brotherlogic@gmail.com>
2023-12-06 09:45:02 -0800

    Server support for part2

--- solvers/2023/src/server.rs
+++ solvers/2023/src/server.rs
@@ -118,6 +118,14 @@ impl SolverService for RServer {
                 big_answer:0,
                 answer:tanswer,
          }));
+         if rq.year == 2023 && rq.day == 6 && rq.part == 2 {
+            let tanswer = day6::solve_day6_part2(rq.data);
+            println!("Returning {}", tanswer);
+            return Ok(Response::new(SolveResponse{
+                string_answer:"".to_string(),
+                big_answer:0,
+                answer:tanswer,
+         }));
         }
        Err(Status::unimplemented("Solution is not implemented yet"))
     }
