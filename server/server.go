79c973d5af01273c2092dfee50ca1b687039bbca
Simon Tucker <brotherlogic@gmail.com>
2023-12-06 17:18:51 -0800

    Fix bucketing for run time histogram

--- server/server.go
+++ server/server.go
@@ -48,8 +48,9 @@ var (
 	}, []string{"puzzle", "result"})
 
 	solveTimes = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "adventofcode_times",
-		Help: "The size of the print queue",
+		Name:    "adventofcode_times",
+		Help:    "The size of the print queue",
+		Buckets: []float64{1, .10, 100, 1000, 2000, 4000, 8000, 16000, 32000, 64000},
 	}, []string{"puzzle", "result"})
 )
 
