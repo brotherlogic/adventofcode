2dfd4f234a3fd36bd7ea7affb85176c93d31f712
Simon Tucker <brotherlogic@gmail.com>
2023-12-09 09:32:03 -0800

    Working on day 9 part2. This closes #465

--- solvers/2023/src/day9.rs
+++ solvers/2023/src/day9.rs
@@ -36,6 +36,16 @@ pub fn solve_day9_part1(data: String) -> i32 {
     return total
 }
 
+pub fn solve_day9_part2(data: String) -> i32 {
+    let mut total: i32 = 0;
+
+    for line in data.split("\n") {
+        total += get_next(line.to_string());
+    }
+
+    return total
+}
+
 #[cfg(test)]
 mod testsca {
     use super::*;
@@ -56,5 +66,16 @@ mod testsca {
 
        let score = solve_day9_part1(test_case);
        assert_eq!(score, 114)
+    }
+
+
+    #[test]
+    fn part2_tests() {
+       let test_case = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45".to_string();
+
+       let score = solve_day9_part2(test_case);
+       assert_eq!(score, 2)
     }
 }
