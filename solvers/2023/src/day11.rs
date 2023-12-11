ba4ace20a9f1792da2e58d8b546244adce5e77e0
Simon Tucker <brotherlogic@gmail.com>
2023-12-11 09:44:12 -0800

    Working on day 11 part 2

--- solvers/2023/src/day11.rs
+++ solvers/2023/src/day11.rs
@@ -1,3 +1,80 @@
+pub fn solve_day11_part2(data: String, adder: i64) -> i64 {
+    let mut sgalaxies = Vec::new();
+
+    let mut max_row: i64 = 0;
+    let mut max_col: i64 = 0;
+    for (posy, line) in data.split("\n").enumerate() {
+        for (posx, c) in line.trim().chars().enumerate() {
+            if c == '#' {
+                sgalaxies.push((posx as i64, posy as i64));
+                if posy as i64 > max_row {
+                    max_row = posy as i64;
+                }
+                if posx as i64 > max_col {
+                    max_col = posx as i64;
+                }
+            }
+        }
+    }
+
+    println!("Found {} galaxies", sgalaxies.len());
+
+    let mut galaxies: Vec<(i64,i64)> = Vec::new();
+    let mut rows = Vec::new();
+    let mut cols = Vec::new();
+    for row in 0..max_row {
+        let mut count = 0;
+        for (gx,gy) in &sgalaxies {
+            if *gy == row {
+                count+=1;
+            }
+        }
+        if count == 0 {
+            rows.push(row);
+        }
+    }
+
+    for col in 0..max_col{
+        let mut count = 0;
+        for (gx,gy) in &sgalaxies {
+            if *gx == col {
+                count+=1;
+            }
+        }
+        if count == 0 {
+            cols.push(col);
+        }
+    }
+
+    for (gx,gy) in &sgalaxies {
+        let mut ngx = *gx;
+        let mut ngy = *gy;
+
+        for col in &cols {
+            if col < gx {
+                ngx+=adder;
+            }
+        }
+
+        for row in &rows {
+            if row < gy {
+                ngy+=adder;
+            }
+        }
+
+        galaxies.push((ngx,ngy));
+    }
+
+    let mut tdist = 0;
+    for (i, (xv,yv)) in galaxies.iter().enumerate() {
+        for (xo,yo) in &galaxies[i..galaxies.len()] {
+            tdist += abs(xv-xo) + abs(yv-yo);
+        }
+    }
+
+    return tdist;
+}
+
 pub fn solve_day11_part1(data: String) -> i64 {
     let mut sgalaxies = Vec::new();
 
@@ -101,5 +178,39 @@ mod testsca {
 
     let score = solve_day11_part1(test_case);
     assert_eq!(score, 374)
+    }
+
+    #[test]
+    fn part2_test_first() {
+    let test_case = "...#......
+    .......#..
+    #.........
+    ..........
+    ......#...
+    .#........
+    .........#
+    ..........
+    .......#..
+    #...#.....".to_string();
+
+    let score = solve_day11_part2(test_case, 100);
+    assert_eq!(score, 8410)
+    }
+
+    #[test]
+    fn part2_test_second() {
+    let test_case = "...#......
+    .......#..
+    #.........
+    ..........
+    ......#...
+    .#........
+    .........#
+    ..........
+    .......#..
+    #...#.....".to_string();
+
+    let score = solve_day11_part2(test_case, 10);
+    assert_eq!(score, 1030)
     }
 }
