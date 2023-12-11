44ceebe4f9e108b05df63cb1e04a17f1b52fbbd6
Simon Tucker <brotherlogic@gmail.com>
2023-12-11 08:54:46 -0800

    Fixed

--- solvers/2023/src/day11.rs
+++ solvers/2023/src/day11.rs
@@ -1,5 +1,85 @@
 pub fn solve_day11_part1(data: String) -> i64 {
-    return 0;
+    let mut sgalaxies = Vec::new();
+
+    let mut max_row: i64 = 0;
+    let mut max_col: i64 = 0;
+    for (posy, line) in data.split("\n").enumerate() {
+        for (posx, c) in line.trim().chars().enumerate() {
+            if c == '#' {
+                sgalaxies.push((posx as i64, posy as i64));
+                if posy as i64 > max_row {
+                    max_row = posy as i64;
+                }
+                if posx as i64 > max_col {
+                    max_col = posx as i64;
+                }
+            }
+        }
+    }
+
+    println!("Found {} galaxies", sgalaxies.len());
+
+    let mut galaxies: Vec<(i64,i64)> = Vec::new();
+    let mut rows = Vec::new();
+    let mut cols = Vec::new();
+    for row in 0..max_row {
+        let mut count = 0;
+        for (gx,gy) in &sgalaxies {
+            if *gy == row {
+                count+=1;
+            }
+        }
+        if count == 0 {
+            rows.push(row);
+        }
+    }
+
+    for col in 0..max_col{
+        let mut count = 0;
+        for (gx,gy) in &sgalaxies {
+            if *gx == col {
+                count+=1;
+            }
+        }
+        if count == 0 {
+            cols.push(col);
+        }
+    }
+
+    for (gx,gy) in &sgalaxies {
+        let mut ngx = *gx;
+        let mut ngy = *gy;
+
+        for col in &cols {
+            if col < gx {
+                ngx+=1;
+            }
+        }
+
+        for row in &rows {
+            if row < gy {
+                ngy+=1;
+            }
+        }
+
+        galaxies.push((ngx,ngy));
+    }
+
+    let mut tdist = 0;
+    for (i, (xv,yv)) in galaxies.iter().enumerate() {
+        for (xo,yo) in &galaxies[i..galaxies.len()] {
+            tdist += abs(xv-xo) + abs(yv-yo);
+        }
+    }
+
+    return tdist;
+}
+
+fn abs(val: i64) -> i64 {
+    if val > 0 {
+        return val;
+    }
+    return 0-val;
 }
 
 #[cfg(test)]
