6d9da7d104fa6e5b756d3f62736f74f1efdcb872
Simon Tucker <brotherlogic@gmail.com>
2023-12-13 22:14:25 -0800

    Small fix

--- solvers/2023/src/day13.rs
+++ solvers/2023/src/day13.rs
@@ -3,10 +3,10 @@ pub fn solve_day13_part1(data: String) -> i32 {
 
     let mut total = 0;
     for board in all {
-        let v = symmetry(board.clone());
+        let v = symmetry(board.clone(), 0);
         let rboard = rotate(board.clone());
 
-        let h = symmetry(rboard.clone());
+        let h = symmetry(rboard.clone(), 0);
         if h > 0 || v > 0 {
             for row in &board {
                 println!("{}", row);
@@ -53,21 +53,37 @@ pub fn solve_day13_part2(data: String) -> i32 {
 
     let mut total = 0;
     for tboard in all {
-        let iv = symmetry(tboard.clone());
-        let ih = symmetry(rotate(tboard.clone()));
-        for board in smudge(tboard) {
+        let iv = symmetry(tboard.clone(), 0);
+        let ih = symmetry(rotate(tboard.clone()), 0);
+        for board in smudge(tboard.clone()) {
            
-            let v = symmetry(board.clone());
+            let v = symmetry(board.clone(), iv);
             let rboard = rotate(board.clone());
 
-            let h = symmetry(rboard.clone());
-            if ih != h || iv != v {
-                println!("GOT {} and {} vs {} and {}", h, v, ih, iv);       
+            let h = symmetry(rboard.clone(), ih);
+
+            println!("FOUND {} and {} given {} and {}", v, h, iv, ih);
+            if (h != 0 || v != 0) && (ih != h || iv != v) {
+                println!("GOT {} and {} vs {} and {}", h, v, ih, iv);    
+
+                println!("TBOARD");
+                for line in tboard {
+                    println!("{}", line);
+                }
+                println!("BOARD");
+                for line in board {
+                    println!("{}", line);
+                }
+                println!("RBOARD");
+                for line in rboard {
+                    println!("{}", line);
+                }
+
                 if iv != v {
-                    println!("RESOLVED TO {}", v);
+                    println!("RESOLVED TO v {}", v);
                     total += v*100;
                 } else {
-                    println!("RESOLVED TO {}", h);
+                    println!("RESOLVED TO h {}", h);
                     total += h;
                 }
                 break;
@@ -92,7 +108,7 @@ fn rotate(board: Vec<String>) -> Vec<String> {
     return nboard;
 }
 
-fn symmetry(board: Vec<String>) -> i32 {
+fn symmetry(board: Vec<String>, not: i32) -> i32 {
     let mut v = 0;
 
     //Vertical
@@ -108,7 +124,7 @@ fn symmetry(board: Vec<String>) -> i32 {
         } else {
             m = false;
         }
-        if m {
+        if m && y as i32 != not{
             v = y as i32;
             break;
         }
@@ -185,26 +201,20 @@ fn part2_test_first() {
 
 #[test]
 fn part2_test_edge() {
-   let test_case = "##.#......#.##.
-   ###........####
-   ###..#..#..####
-   .###..##..###.#
-   .#.###..###...#
-   .....####.....#
-   #.###.##.###.##
-   #............#.
-   ..###.##.###..#
-   ##.#.#..#.#.###
-   ...#..##..#....
-   ##.#.####.#.##.
-   ###.#.##.#.###.
-   ....######.....
-   ####......#####
-   ####......#####
-   ....######.....".to_string();
+   let test_case = "##.#.#.#.###...#.
+   .##.#...#.###.#..
+   ###.######..#....
+   ###.######..#....
+   .##.#...#.###.#..
+   ##.#.#.#.###...#.
+   .##.#####.##..#..
+   ..##..###.#..#.##
+   ###..####.##.#..#
+   ###..####.##.#..#
+   ..##..###.##.#.##".to_string();
 
    let score = solve_day13_part2(test_case);
-   assert_eq!(score, 7)
+   assert_eq!(score, 5125)
 }
 }
 
