fbf98735a83b33a4206838842598a3b128f3bcfa
Simon Tucker <brotherlogic@gmail.com>
2024-11-11 16:54:27 -0800

    Adding c sharp solver

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 python3 -m grpc_tools.protoc -I./proto/ --python_out=./solvers/2015 --pyi_out=. --grpc_python_out=./solvers/2015 proto/advent.proto
 protoc --proto_path ../../../ -I=./proto --go_out ./proto --go_opt=paths=source_relative --go-grpc_out ./proto --go-grpc_opt paths=source_relative --go-grpc_opt=require_unimplemented_servers=false proto/advent.proto
+protoc --proto_path ./ --csharp_out ./proto --plugin=protoc-gen-grpc --grpc_out=./proto proto/advent.proto
