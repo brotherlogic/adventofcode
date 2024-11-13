39a4566258b48d177de44e2d73ec025a69fac01c
Simon Tucker <brotherlogic@gmail.com>
2024-11-12 20:35:38 -0800

    Adds ruby support

---
+++ solvers/2024/server.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+this_dir = File.expand_path(File.dirname(__FILE__))
+lib_dir = File.join(this_dir, '../../proto')
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
+
+require 'grpc'
+require 'advent_services_pb'
+
+class SolverServer < Adventofcode::SolverService::Service
+    # say_hello implements the SayHello rpc method.
+    def solve(solve_req, _unused_call)
+      Adventofcode::SolveResponse.new(solution: 1)
+    end
+  end
+
+  def main
+    s = GRPC::RpcServer.new
+    s.add_http2_port('0.0.0.0:8080', :this_port_is_insecure)
+    s.handle(SolverServer)
+    # Runs the server with SIGHUP, SIGINT and SIGTERM signal handlers to
+    #   gracefully shutdown.
+    # User could also choose to run server via call to run_till_terminated
+    s.run_till_terminated_or_interrupted([1, 'int', 'SIGTERM'])
+  end
+  
+  main
