96dacc9451acdcf0711f3c8c68690d45dc3274fd
Simon <brotherlogic@gmail.com>
2024-11-13 06:46:09 -0800

    Runs the checks

--- solvers/2024/server.rb
+++ solvers/2024/server.rb
@@ -7,6 +7,19 @@ $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 require 'grpc'
 require 'advent_services_pb'
 
+class Registrar
+  def Register()
+    hostname = "adventofcode.adventofcode:8082"
+    stub = Adventofcode::AdventOfCodeInternalService::Stub.new(hostname, :this_channel_is_insecure)
+    begin
+      message = stub.Register(Adventofcode::RegisterRequest.new(year: 2024)).message
+      p "Greeting: #{message}"
+    rescue GRPC::BadStatus => e
+      abort "ERROR: #{e.message}"
+    end
+  end
+end
+
 class SolverServer < Adventofcode::SolverService::Service
     def solve(solve_req, _unused_call)
       Adventofcode::SolveResponse.new(solution: 1)
@@ -18,15 +31,8 @@ class SolverServer < Adventofcode::SolverService::Service
     s.add_http2_port('0.0.0.0:8080', :this_port_is_insecure)
     s.handle(SolverServer)
 
-    hostname = "adventofcode.adventofcode:8082"
-    stub = Adventofcode::AdventOfCodeInternalService::Stub.new(hostname, :this_channel_is_insecure)
-    begin
-      message = stub.Register(Adventofcode::RegisterRequest.new(year: 2024)).message
-      p "Greeting: #{message}"
-    rescue GRPC::BadStatus => e
-      abort "ERROR: #{e.message}"
-    end
-  
+    registar = Registrar.new
+    registar.Register()
 
     # Runs the server with SIGHUP, SIGINT and SIGTERM signal handlers to
     #   gracefully shutdown.
