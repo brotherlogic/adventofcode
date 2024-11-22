748d83756489599601b76f0ba0b719924be62535
Simon <brotherlogic@gmail.com>
2024-11-21 16:17:34 -0800

    Adds some testing infrastructure for 2024

---
+++ solvers/2024/day0.rb
@@ -0,0 +1,9 @@
+class Day0
+    def solvePart1(data)
+        return 10
+    end
+
+    def solvePart2(data)
+        return 15
+    end
+end
---
+++ solvers/2024/day0_spec.rb
@@ -0,0 +1,19 @@
+# day0_spec.rb
+require 'rspec'
+require_relative 'day0'
+
+RSpec.describe Day0 do
+  describe "part 0" do
+    it "returns 10" do
+      day0 = Day0.new
+      expect(day0.solvePart1("")).to eq(11)
+    end
+  end
+  describe "part 1" do
+    it "returns 15" do
+      day0 = Day0.new
+      expect(day0.solvePart2("")).to eq(15)
+    end
+  end
+
+end
