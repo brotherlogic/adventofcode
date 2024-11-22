eb4a85a342bf83c5d351bc89d683fa28c155e37d
Simon <brotherlogic@gmail.com>
2024-11-21 16:42:03 -0800

    Try to test against the server, rather than just the solver class

--- solvers/2024/day0_spec.rb
+++ solvers/2024/day0_spec.rb
@@ -1,12 +1,13 @@
 # day0_spec.rb
 require 'rspec'
 require_relative 'day0'
+require_relative 'server'
 
 RSpec.describe Day0 do
   describe "part 0" do
     it "returns 10" do
-      day0 = Day0.new
-      expect(day0.solvePart1("")).to eq(10)
+      day0 = SolverServer.new
+      expect(day0.solve(Adventofcode::SolveRequest.new(year:2024, day: 0))).to eq(10)
     end
   end
   describe "part 1" do
@@ -15,5 +16,4 @@ RSpec.describe Day0 do
       expect(day0.solvePart2("")).to eq(15)
     end
   end
-
 end
