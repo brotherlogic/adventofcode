9f611d9bd002d3042cabf20156de5310b9914112
Simon <brotherlogic@gmail.com>
2024-12-01 05:06:40 -0800

    Adds day1 ruby template. This closes #609

---
+++ solvers/2024/day1.rb
@@ -0,0 +1,5 @@
+class Day1
+    def solvePart1(solve_req)
+        return 5
+    end
+end
---
+++ solvers/2024/day1_spec.rb
@@ -0,0 +1,12 @@
+# day1_spec.rb
+require 'rspec'
+require_relative 'day1'
+
+RSpec.describe Day0 do
+  describe "part 1" do
+    it "returns 10" do
+      day1 = Day1.new
+      expect(day1.solve(Adventofcode::SolveRequest.new(year:2024, day: 0))).to eq(10)
+    end
+  end
+end
