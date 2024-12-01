d20b5d9f7418edb0830f90d47ec29d780e8fc4aa
Simon <brotherlogic@gmail.com>
2024-12-01 05:38:35 -0800

    Fix name

--- solvers/2024/day1_spec.rb
+++ solvers/2024/day1_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Day1 do
   describe "part 1" do
     it "returns 10" do
       day1 = Day1.new
-      expect(day1.solve(Adventofcode::SolveRequest.new(year:2024, day: 0))).to eq(10)
+      expect(day1.solvePart1(Adventofcode::SolveRequest.new(year:2024, day: 0))).to eq(10)
     end
   end
 end
