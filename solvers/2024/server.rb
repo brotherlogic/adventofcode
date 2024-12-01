d71acb7fded162c0752eebb581405dbe3576e20b
Simon Tucker <brotherlogic@gmail.com>
2024-12-01 11:40:56 -0800

    Also log result

--- solvers/2024/server.rb
+++ solvers/2024/server.rb
@@ -13,7 +13,8 @@ class Registrar
     hostname = "adventofcode.adventofcode:8082"
     stub = Adventofcode::AdventOfCodeInternalService::Stub.new(hostname, :this_channel_is_insecure)
     begin
-      stub.register(Adventofcode::RegisterRequest.new(callback: "adventofcode-solver-2024.adventofcode:8080", year: 2024))
+      result = stub.register(Adventofcode::RegisterRequest.new(callback: "adventofcode-solver-2024.adventofcode:8080", year: 2024))
+      puts result
     rescue GRPC::BadStatus => e
       abort "Register Error: #{e.message}"
     end
