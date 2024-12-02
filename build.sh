04dc6d03ce7e9aa1f49ab7587e24f8f228fbb95a
Simon Tucker <brotherlogic@gmail.com>
2024-12-01 21:05:27 -0800

    Aha

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-python3 -m grpc_tools.protoc -I./proto/ --python_out=./solvers/2015 --pyi_out=. --grpc_python_out=./solvers/2015 proto/advent.proto
+#python3 -m grpc_tools.protoc -I./proto/ --python_out=./solvers/2015 --pyi_out=. --grpc_python_out=./solvers/2015 proto/advent.proto
 protoc --proto_path ../../../ -I=./proto --go_out ./proto --go_opt=paths=source_relative --go-grpc_out ./proto --go-grpc_opt paths=source_relative --go-grpc_opt=require_unimplemented_servers=false proto/advent.proto
-protoc --proto_path ./ --csharp_out ./proto --plugin=protoc-gen-grpc --grpc_out=./proto proto/advent.proto
-grpc_tools_ruby_protoc -I ./proto --ruby_out ./proto --grpc_out ./proto proto/advent.proto
+#protoc --proto_path ./ --csharp_out ./proto --plugin=protoc-gen-go-grpc --grpc_out=./proto proto/advent.proto
+#grpc_tools_ruby_protoc -I ./proto --ruby_out ./proto --grpc_out ./proto proto/advent.proto
