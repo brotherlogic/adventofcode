7e62a65427308610b565cb1429792176c3b0224b
Simon <brotherlogic@gmail.com>
2024-12-05 06:23:18 -0800

    Added day 5 files

---
+++ solvers/2024/day5.rb
@@ -0,0 +1,5 @@
+class Day5
+    def solvePart1(solve_req)
+        return 0
+    end
+end
---
+++ solvers/2024/day5_spec.rb
@@ -0,0 +1,41 @@
+# day1_spec.rb
+require 'rspec'
+require_relative 'day5'
+require_relative 'lib/advent_pb'
+
+RSpec.describe Day5 do
+  describe "part 1" do
+    it "returns 143" do
+      day5 = Day5.new
+      data = "47|53
+      97|13
+      97|61
+      97|47
+      75|29
+      61|13
+      75|53
+      29|13
+      97|29
+      53|29
+      61|53
+      97|53
+      61|29
+      47|13
+      75|47
+      97|75
+      47|61
+      75|61
+      47|29
+      75|13
+      53|13
+      
+      75,47,61,53,29
+      97,61,53,29,13
+      75,29,13
+      75,97,47,61,53
+      61,13,29
+      97,13,75,29,47"
+      expect(day5.solvePart1(Adventofcode::SolveRequest.new(data: data))).to eq(143)
+    end
+  end
+end
