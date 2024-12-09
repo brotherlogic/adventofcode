21d04c80ad1bad77c5f4a0ecdc9f80e6dffc02ca
Simon Tucker <brotherlogic@gmail.com>
2024-12-09 12:40:12 -0800

    Adds solution for part 1

--- solvers/2024/day8.rb
+++ solvers/2024/day8.rb
@@ -18,6 +18,28 @@ class Day8
         return map, finds
     end
 
+    def addToMap(x, y, nx, ny, finds)
+        print x,",",y, " -> ",nx,",",ny,"\n"
+
+        a1x = x-(nx-x)
+        a1y = y-(ny-y)
+
+        if a1x >= 0 && a1y >= 0 && a1y < finds.length() && a1x < finds[0].length()
+            finds[a1y][a1x] = "#"
+        end
+
+        print "A1 ", a1x, ",", a1y, "\n"
+
+        a2x = nx+(nx-x)
+        a2y = ny+(ny-y)
+
+        if a2x >= 0 && a2y >= 0 && a2y < finds.length() && a2x < finds[0].length()
+            finds[a2y][a2x] = "#"
+        end
+
+        print "A2 ", a2x,",",a2y,"\n"
+    end
+
     def solvePart1(solve_req)
        map, finds = buildMap(solve_req.data)
 
@@ -25,7 +47,6 @@ class Day8
        for y in 0..map.length()-1
             for x in 0..map[y].length()-1
                 if map[y][x] != "."
-                    print "SEARCHING ", x, ",", y, "\n"
                     # Found an antenna, look for other ones
                     for ny in y..map.length()-1
                         for nx in 0..map[y].length()-1
