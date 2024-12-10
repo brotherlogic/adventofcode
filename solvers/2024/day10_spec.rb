ab7b3d3f34a9a6ddd69770ef2947aac160bee1c0
Simon Tucker <brotherlogic@gmail.com>
2024-12-10 10:12:50 -0800

    Also part 2

--- solvers/2024/day10_spec.rb
+++ solvers/2024/day10_spec.rb
@@ -18,4 +18,18 @@ RSpec.describe Day10 do
       expect(day10.solvePart1(Adventofcode::SolveRequest.new(data: data))).to eq(36)
     end
   end
+  describe "part 2" do
+    it "returns 81" do
+      day10 = Day10.new
+      data = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"
+      expect(day10.solvePart2(Adventofcode::SolveRequest.new(data: data))).to eq(81)
+    end
+  end
 end
