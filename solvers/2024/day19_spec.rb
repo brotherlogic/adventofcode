c28fee9482f8a6759028bbbdb367809336ba4e4c
Simon <brotherlogic@gmail.com>
2024-12-19 21:22:56 -0800

    Working on part 2

--- solvers/2024/day19_spec.rb
+++ solvers/2024/day19_spec.rb
@@ -17,7 +17,23 @@ RSpec.describe Day19 do
       bwurrg
       brgr
       bbrgwb"
-      expect(day19.solvePart1(Adventofcode::SolveRequest.new(data: data))).to eq(6)
+      #expect(day19.solvePart1(Adventofcode::SolveRequest.new(data: data))).to eq(6)
+    end
+  end
+  describe "part 2" do
+    it "returns 16" do
+      day19 = Day19.new
+      data = "r, wr, b, g, bwu, rb, gb, br
+
+      brwrr
+      bggr
+      gbbr
+      rrbgbr
+      ubwu
+      bwurrg
+      brgr
+      bbrgwb"
+      expect(day19.solvePart2(Adventofcode::SolveRequest.new(data: data))).to eq(16)
     end
   end
 end
