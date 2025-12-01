f4e878eec7c5793f21aa81f057ef2b908bcad50e
Simon Tucker <brotherlogic@gmail.com>
2025-12-01 14:39:21 -0800

    Adds part 2

--- solvers/2025/day1_test.go
+++ solvers/2025/day1_test.go
@@ -27,3 +27,24 @@ L82`
 		t.Fatalf("Bad answer: %v", resp.GetAnswer())
 	}
 }
+
+func TestDay1Part2(t *testing.T) {
+	data := `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+	server := &Server{}
+	resp, err := server.Day1Part2(context.Background(), &pb.SolveRequest{Data: data})
+	if err != nil {
+		t.Fatalf("Unable to solve: %v", err)
+	}
+	if resp.GetAnswer() != 6 {
+		t.Fatalf("Bad answer: %v", resp.GetAnswer())
+	}
+}
