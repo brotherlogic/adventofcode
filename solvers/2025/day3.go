7eef8499a014cbdee9f6a3e496d3c62a1f8687e7
brotherlogic <brotherlogic@gmail.com>
2025-12-03 06:33:08 -0800

    Fixed for test data in part 1

--- solvers/2025/day3.go
+++ solvers/2025/day3.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"context"
+	"strconv"
+	"strings"
 
 	pb "github.com/brotherlogic/adventofcode/proto"
 )
 
+func findBiggest(num string, offset, cutoff int) (int64, int) {
+	biggest := "0"[0]
+	bo := 0
+	for i := offset; i <= cutoff; i++ {
+		if num[i] > biggest {
+			biggest = num[i]
+			bo = i
+		}
+	}
+
+	v, _ := strconv.ParseInt(string(biggest), 10, 64)
+	return v, bo
+}
+
 func (s *Server) Day3Part1(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
-	return &pb.SolveResponse{Answer: 1}, nil
+	sum := int32(0)
+	for _, line := range strings.Split(req.GetData(), "\n") {
+		bv, bo := findBiggest(strings.TrimSpace(line), 0, len(line)-2)
+
+		sv, _ := findBiggest(strings.TrimSpace(line), bo+1, len(line)-1)
+		//log.Printf("Found %v and %v", bv, sv)
+		sum += int32(bv*10 + sv)
+	}
+
+	return &pb.SolveResponse{Answer: sum}, nil
 }
