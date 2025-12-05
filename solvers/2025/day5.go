b0a6ba4e5b51f422310ea4eeabb0e876c7efad5b
Simon Tucker <brotherlogic@gmail.com>
2025-12-05 15:52:54 -0800

    Adds day 5 files

---
+++ solvers/2025/day5.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/brotherlogic/adventofcode/proto"
+)
+
+func (s *Server) Day5Part1(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
+	return &pb.SolveResponse{
+		Answer: 0,
+	}, nil
+}
---
+++ solvers/2025/day5_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	pb "github.com/brotherlogic/adventofcode/proto"
+)
+
+const (
+	day5 = `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`
+)
+
+func TestDay5Part1(t *testing.T) {
+	s := &Server{}
+
+	res, err := s.Day5Part1(context.Background(), &pb.SolveRequest{
+		Data: day5,
+	})
+
+	if err != nil {
+		t.Fatalf("Unable to solve: %v", err)
+	}
+
+	if res.GetAnswer() != 3 {
+		t.Errorf("Expected 3, got %v", res.GetAnswer())
+	}
+}
