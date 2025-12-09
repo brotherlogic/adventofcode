67c8b8c6f5348f70b3230f5624e7e489af2251c3
brotherlogic <brotherlogic@gmail.com>
2025-12-08 17:09:08 -0800

    Stash

---
+++ solvers/2025/day8.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"context"
+
+	pb "github.com/brotherlogic/adventofcode/proto"
+)
+
+func (s *Server) Day8Part1(ctx context.Context, req *pb.SolveRequest) (*pb.SolveResponse, error) {
+	// Implementation goes here
+	return &pb.SolveResponse{Answer: 0}, nil
+}
